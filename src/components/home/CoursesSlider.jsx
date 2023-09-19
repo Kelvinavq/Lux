d51336d71639132452140e913f41372f35a39c24
Kelvin <kvalera200244@gmail.com>
2023-09-18 22:26:32 -0400

    up 4

--- src/components/home/CoursesSlider.jsx
+++ src/components/home/CoursesSlider.jsx
@@ -7,12 +7,12 @@ import { Translation } from "../../Translation";
 import "../../styles/coursesSlider.css";
 
 import imgCoaching from "../../assets/courses/coaching_ventas.jpeg";
-import imgIngles from "../../assets/courses/ingles.jpg";
 import imgDropshipping from "../../assets/courses/dropshipping.jpg";
 import imgOratoria from "../../assets/courses/oratoria.jpg";
 import imgTrading from "../../assets/courses/trading.jpg";
 import imgMarketing from "../../assets/courses/marketing_digital.jpeg";
 import imgEcommerce from "../../assets/courses/ecommerce.jpg";
+import imgIngles from "../../assets/courses/ingles.jpg"
 
 
 import AOS from "aos";
